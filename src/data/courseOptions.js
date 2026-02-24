357cbd6deea4e46feb2e7aafc1f6817c2a98d066
Udeme01 <emmanueluddy2@gmail.com>
2026-02-24 21:39:40 +0100

    added courses to drop down

--- src/data/courseOptions.js
+++ src/data/courseOptions.js
@@ -4,22 +4,6 @@ export const courseOptions = [
     label: "Frontend Development",
   },
   {
-    value: "ui/ux design",
-    label: "UI/UX Design",
-  },
-  {
-    value: "data analysis",
-    label: "Data Analysis",
-  },
-  {
-    value: "backend development",
-    label: "Backend Development",
-  },
-  {
-    value: "cybersecurity",
-    label: "Cybersecurity",
-  },
-  {
     value: "graphics design",
     label: "Graphics Design",
   },
