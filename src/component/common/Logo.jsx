ecd1d80fa875560267fb931ffe330fce9c48b706
Udeme01 <emmanueluddy2@gmail.com>
2026-02-24 21:48:57 +0100

    changed logo size

--- src/component/common/Logo.jsx
+++ src/component/common/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <section className="w-[80%] mx-auto">
       <h1 className="text-4xl xl:text-6xl tracking-tighter text-green-100 font-bold z-50">
-        <img src={logo} alt="The Genesis Lab logo" className="absolute top-0 z-50 left-0 w-64 md:w-80 h-fit" />
+        <img src={logo} alt="The Genesis Lab logo" className="absolute top-0 z-50 left-0 w-56 md:w-80 h-fit" />
       </h1>
     </section>
   );
